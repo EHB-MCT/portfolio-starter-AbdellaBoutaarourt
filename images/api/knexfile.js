9dd6b7d6ca9861f48711ee89139230629da802bd
Abdella Boutaarourt <abdella.boutaarourt@student.ehb.be>
2024-01-04 08:19:53 +0100

    knex fix beug

--- images/api/knexfile.js
+++ images/api/knexfile.js
@@ -7,8 +7,12 @@ require('dotenv').config({path: '../../.env'});
 module.exports = {
 
   development: {
-    client: "pg",
-    connection: process.env.PG_CONNECTION_STRING,
+    client: 'postgresql',
+    connection: {
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
+    },
     migrations: {
       tableName: 'knex_migrations',
       directory: "./migrations",
